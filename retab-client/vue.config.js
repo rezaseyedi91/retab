3369ea1739de71db9f44ce62a3d5d2a417eddddf
Reza Seyedi <reza.seyedi010@gmail.com>
2026-09-05 12:18:03 +0330

    debugging docker

--- retab-client/vue.config.js
+++ retab-client/vue.config.js
@@ -5,12 +5,12 @@ dotenv.config({});
 
 /** @type {import('vue').AppConfig} */
 module.exports = defineConfig({
-  
+
   transpileDependencies: true,
   devServer: {
     port: process.env.VUE_APP_DEV_PORT,
   },
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
-  // indexPath: process.env.VUE_APP_INDEX_PATH
+  // indexPath: process.env.VUE_APP_IND EX_PATH
   
 })
